0f9c61f875a1f66f945cb3326b86ba01b2832152
shelter-hosea <shelter.hosea@gmail.com>
2016-08-19 14:18:33 +0100

    develeoping

---
+++ noteApplication1.js
@@ -0,0 +1,31 @@
+
+class NoteApplicaiton {
+
+	constructor(author) {	
+
+	this.author = author ;
+	this.noteList = [] ;
+	
+}
+create (note_content) {
+
+	this.noteList.push(note_content)
+}
+
+
+listNotes() {
+
+	for (var note_id = 0; note_id< note_id.length; ++note_id) {
+		
+		console.log("Note ID: ",+ [note_id] );
+		console.log("By Athot" + this.author);
+	}
+	
+}
+
+get(note_id) {
+	return this.noteslist[note_id];
+}
+
+search(search_text)
+}
