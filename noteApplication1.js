98f02e4509ecc2f7129dfd8693c83be434213901
shelter-hosea <shelter.hosea@gmail.com>
2016-08-20 01:08:56 +0100

    indentation adjusted

--- noteApplication1.js
+++ noteApplication1.js
@@ -9,29 +9,34 @@ class NoteApplicaiton {
 
 	constructor(author) {	
 
-	this.author = author ;
-	this.noteList = [] ;
+		this.author = author ;
+		this.noteList = [] ;
 	
-}
-create (note_content) {
+	}
+	create (note_content) {
 
-	this.noteList.push(note_content)
-}
+		this.noteList.push(note_content)
+	}
 
 
-listNotes() {
+	listNotes() {
 
-	for (var note_id = 0; note_id< note_id.length; ++note_id) {
+		for (var note_id = 0; note_id< note_id.length; ++note_id) {
 		
-		console.log("Note ID: ",+ [note_id] );
-		console.log("By Author" + this.author);
+			console.log("Note ID: ",+ [note_id] );
+			console.log("By Author" + this.author);
 	}
 	
-}
 
-get(note_id) {
-	return this.noteslist[note_id];
-}
+	get(note_id) {
+	
+		return this.noteslist[note_id];
+	}
 
-search(search_text)
+	search(search_text) {
+		
+
+		
+	}
+
 }
