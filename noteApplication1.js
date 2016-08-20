2d2a4905f22ceb36d2c661ed5135bc28d0a0fba5
shelter-hosea <shelter.hosea@gmail.com>
2016-08-20 06:46:43 +0100

    modification
    a lot of changes

--- noteApplication1.js
+++ noteApplication1.js
@@ -5,44 +5,80 @@
      /********************************************************************************************************************/
 
 
-class NoteApplicaiton {
-
-	constructor(author) {	
-
-		this.author = author ;
-		this.noteList = [] ;
-	
+ 
+	 class Note {
+	constructor(note_content, author){
+		this.author = author.toUpperCase(author);
+		this.note_content = note_content;
 	}
-	create (note_content) {
-
-		this.noteList.push(note_content);
+ }
+ 
+ 
+ class NoteApplication {
+ 	
+ 	constructor(author) {
+		this.author = author;
+		this.notes = [];
 	}
-
-
-	listNotes() {
-
-		for (var note_id = 0; note_id< note_id.length; ++note_id) {
+ 	
+ 	create(note_content) {
+ 		var note = new Note(note_content, this.author);
+ 		console.log("");
+		this.notes.push(note);
 		
-			console.log("Note ID: ",+ [note_id] );
-			console.log("By Author" + this.author);
-		}
-	}
-
-	get_note(note_id) {
-	
-		return this.noteslist[note_id];
-	}
-
-	search(search_text) {
-		for(let i in this.notes){
-			if(search_text === this.notes[i]) {
-				console.log('Showing result for search: ' + search_text)
-				console.log('Note ID: ' + i)
-				console.log(this.notes[i])
-				console.log('BY Author ' + this.author)
-				}
+ 	}
+ 	
+ 	listNotes() {
+ 	
+		for(i = 0; i < this.notes.length; i++){
+			console.log("Note_ID " + (i+1));
+			console.log([this.notes[i].note_content]);
+			console.log(" ")
+			console.log("By : ", this.notes[i].author);
+			console.log("");
+			console.log("");
+		} 
+ 	}
+ 
+ get(note_id){
+		if (this.notes[note_id])
+		 return this.notes[note_id];
+		else 
+		 return console.log("Epmty page!, Enter a  page lower number");	
+ 	}
+ 
+ 	search(search_text){
+		var filtered = this.notes.filter(function (note) {
+			//return search_text != -1;
+			if (note.note_content.indexOf(search_text) !== -1) {
+				return note.note_content.indexOf(search_text) !== -1;
 			}
+			else return console.log ("character not found in note");
+		});
+		console.log(filtered);
+	}
+	
+	 delete(note_id){
+	 	
+	 	console.log("");
+		console.log("");
+	 	console.log(this.notes[note_id]);
+	 	console.log("");
+		console.log("");
+	 }
+	// 	deleted_page = this.note[note_id];
 		
+	// 	if (this.notes.splice(note_id) ){
+	// 	this.notes.splice(note_id);
+		
+	// 	console.log ([deleted_word] + "was suscessful deleted");
+	// 	}
+	// 	else console.log("Page was blank");
+	// }
+ //}
+ 
+ 
+ }
 		
 	}
 	
